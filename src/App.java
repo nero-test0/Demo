f40d2b19811371662e90982b968d584d1d8a4c21
Narayan Maity <nero.personalrepo@gmail.com>
2022-12-21 18:40:20 +0530

    Program to filter a stream of integers
    1. This program takes a list of data and puts it into a stream.
    2. The stream of integers are then filtered and only integers are
       allowed to pass which are both divisible by 2 and 3.
    3. For each filtered integer the program prints the int onto a newline.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 10; i < 100; i++) {
+           list.add(i); 
+        }
+        list.stream()
+            .filter(p -> p % 2 == 0)
+            .filter(p -> p % 3 == 0)
+            .forEach(System.out::println);
     }
 }
