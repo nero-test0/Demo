78476aa4edc49ee0ea5b040b05e84544b40fe670
Narayan Maity <nero.personalrepo@gmail.com>
2022-12-22 11:06:33 +0530

    Add Streams file
    Streams file takes any character file as input and displays the output
    of the file onto the console

---
+++ src/Streams.java
@@ -0,0 +1,18 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Streams {
+    public static void main(String[] args) throws IOException {
+       Reader reader = new FileReader("/home/narayan/file"); 
+        int length;
+        char[] charBuff = new char[10];
+        while((length=reader.read(charBuff)) >= 0) {
+            for (int i = 0; i < length; i++) {
+                char charVal = charBuff[i];
+                System.out.print((charVal));
+            }
+        }
+        reader.close();
+    }
+}
