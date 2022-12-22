ee7b0e0d2cc4f845a872ed85a09466cae2790eac
Narayan Maity <79889069+narayan-repo@users.noreply.github.com>
2022-12-22 11:38:56 +0530

    Update Streams.java
    Add comment for testing purpose

--- src/Streams.java
+++ src/Streams.java
@@ -6,7 +6,7 @@ public class Streams {
     public static void main(String[] args) throws IOException {
        Reader reader = new FileReader("/home/narayan/file"); 
         int length;
-        char[] charBuff = new char[10];
+        char[] charBuff = new char[10]; //using char array as a buffer
         while((length=reader.read(charBuff)) >= 0) {
             for (int i = 0; i < length; i++) {
                 char charVal = charBuff[i];
